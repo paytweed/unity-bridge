359aa0087021b9634248d4a40c938774d8db2f40
Harel <harel@paytweed.com>
2023-06-08 16:52:15 +0300

    split tar file

--- inject.sh
+++ inject.sh
@@ -5,6 +5,9 @@ cs=./tweed_assets/TweedUnityHandler.cs
 
 unity_assets_path=./unity/example/Assets
 unity_plugins_path=Plugins
+example_path=./unity/example.tgz
 
+cat example[a-z] > example.tgz
+tar -xvf $example_path
 cp $jslib "$unity_assets_path/$unity_plugins_path"
 cp $cs $unity_assets_path
