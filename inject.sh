011135dde2ba6b19dcd54f2efec2447efc5afb13
Harel <harel@paytweed.com>
2023-06-08 17:19:33 +0300

    update inject.sh

--- inject.sh
+++ inject.sh
@@ -4,10 +4,10 @@ jslib=./tweed_assets/tweedJsSDK.jslib
 cs=./tweed_assets/TweedUnityHandler.cs
 
 unity_assets_path=./unity/example/Assets
-unity_plugins_path=Plugins
+unity_plugins_path=plugins
 example_path=./unity/example.tgz
 
-cat example[a-z] > example.tgz
+cat ./unity/example.tar.tgz* > $example_path
 tar -xvf $example_path
-cp $jslib "$unity_assets_path/$unity_plugins_path"
-cp $cs $unity_assets_path
+cp $jslib ./example/Assets/plugins
+cp $cs ./example/Assets
