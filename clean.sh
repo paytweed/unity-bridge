0ec4dd2fb4b8047d220281457d2e5386e5522dbb
Udi Hakim <udi@paytweed.com>
2023-06-09 17:52:47 +0300

    puthing the clean into its own script

---
+++ clean.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+  rm -f ./fe/src/tweedIntegration.js && rm -Rf ./unity/deploy/*/ && rm -f ./fe/src/tweedIntegration && rm -Rf ./unity/example && rm -f ./unity/example.tgz && rm -Rf ./node_modules && rm -Rf ./fe/node_modules
