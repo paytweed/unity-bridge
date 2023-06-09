64d721036d6e6e7b977e6f1236e746d246e47874
Udi Hakim <udi@paytweed.com>
2023-06-10 01:34:01 +0300

    better clean

--- clean.sh
+++ clean.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-  rm -f ./fe/src/TweedSDK.js && rm -Rf ./unity/deploy/*/ && rm -Rf ./unity/example && rm -f ./unity/example.tgz && rm -Rf ./node_modules && rm -Rf ./fe/node_modules && rm -f ./unity/example.tar.bz2
+  rm -f ./fe/src/TweedSDK.js
+  rm -Rf ./unity/deploy/*/
+  rm -Rf ./unity/example 
+  rm -f ./unity/example.tgz
+  rm -Rf ./node_modules
+  rm -Rf ./fe/node_modules
+  rm -f ./unity/example.tar.bz2
