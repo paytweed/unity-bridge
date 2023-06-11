ef36fc9787b1372a8ae4802e8dc22708c64e723f
Harel <harel@paytweed.com>
2023-06-11 11:56:40 +0300

    display error message

--- fe/src/unityPreloader.js
+++ fe/src/unityPreloader.js
@@ -1,6 +1,6 @@
 var canvas = document.querySelector("#unity-canvas");
-canvas.style.width = "99vw";
-canvas.style.height = "99vh";
+canvas.style.width = "100%";
+canvas.style.height = "100%";
 
 var buildUrl = "http://127.0.0.1:3010/unity";
 var loaderUrl = buildUrl + "/loader";
@@ -17,6 +17,23 @@ script.src = loaderUrl;
 script.onload = () =>
   createUnityInstance(canvas, config).then((unityInstance) => {
     _unityInstance = unityInstance;
+    if (!_unityInstance) {
+      const errMsg = document.createElement("p");
+      errMsg.textContent =
+        "Could not create Unity Instance, you should probably run npm deploy and compile your WebGL content";
+      errMsg.style.zIndex = "1000";
+      errMsg.style.color = "#FF9494";
+      errMsg.style.fontSize = "20px";
+      errMsg.style.margin = "0px";
+      errMsg.style.position = "fixed";
+      errMsg.style.bottom = "30px";
+      errMsg.style.left = "30px";
+
+      document.body.appendChild(errMsg);
+      console.log(
+        "Could not create Unity Instance, you should probably run npm deploy and compile your WebGL content"
+      );
+    }
   });
 document.body.appendChild(script);
 
@@ -25,7 +42,3 @@ function getUnityInstance() {
 }
 
 window.getUnityInstance = getUnityInstance;
-
-if(!_unityInstance) {
-  console.log("Could not create Unity Instance, you should probably run npm deploy and compile your WebGL content")
-}
